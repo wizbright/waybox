64db394085455f0584a26fdc2781037f7fb8a220
Keith Bowes <keithbowes@users.noreply.github.com>
2020-02-18 15:41:39 -0500

    Lifted support for keys from tinywl; TODO: don't have all the code in server.c

--- include/waybox/seat.h
+++ include/waybox/seat.h
@@ -1,16 +1,28 @@
 #ifndef _WB_SEAT_H
 #define _WB_SEAT_H
 
+#include <wlr/types/wlr_keyboard.h>
 #include <wlr/types/wlr_seat.h>
 
 #include "waybox/server.h"
 
 struct wb_seat {
 	struct wlr_seat * seat;
+
+	struct wl_list keyboards;
+	struct wl_listener new_input;
+};
+
+struct wb_keyboard {
+	struct wl_list link;
+	struct wb_server * server;
+	struct wlr_input_device * device;
+
+	struct wl_listener modifiers;
+	struct wl_listener key;
 };
 
 struct wb_server;
 struct wb_seat * wb_seat_create(struct wb_server * server);
-
 void wb_seat_destroy(struct wb_seat * seat);
 #endif
--- waybox/seat.c
+++ waybox/seat.c
@@ -4,8 +4,13 @@
 
 struct wb_seat * wb_seat_create(struct wb_server * server) {
 	struct wb_seat * seat = malloc(sizeof(struct wb_seat));
+
+	wl_list_init(&seat->keyboards);
+	//seat->new_input.notify = new_input_notify;
+	//wl_signal_add(&server->backend->events.new_input, &server->new_input);
 	seat->seat = wlr_seat_create(server->wl_display, "seat0");
 	wlr_seat_set_capabilities(seat->seat, WL_SEAT_CAPABILITY_POINTER | WL_SEAT_CAPABILITY_KEYBOARD);
+
 	return seat;
 }
 
