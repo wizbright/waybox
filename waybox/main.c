a495d1e8e9a5405e156bca081d3d367cd9cbc15e
Keith Bowes <keithbowes@users.noreply.github.com>
2020-02-26 10:31:22 -0500

    Style nits

--- waybox/main.c
+++ waybox/main.c
@@ -5,23 +5,19 @@
 
 #include "waybox/server.h"
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
 	char *startup_cmd = NULL;
 	if (argc > 0) {
 		int i;
-		for (i = 0; i < argc; i++)
-		{
+		for (i = 0; i < argc; i++) {
 			if (!strcmp("--debug", argv[i]) || !strcmp("-v", argv[i]) || !strcmp("--exit", argv[i])) {
 				printf("Warning: option %s is currently unimplemented\n", argv[i]);
-			}
-			else if ((!strcmp("--startup", argv[i]) || !strcmp("-s", argv[i])) && i < argc) {
+			} else if ((!strcmp("--startup", argv[i]) || !strcmp("-s", argv[i])) && i < argc) {
 				startup_cmd = argv[i + 1];
-			}
-			else if (!strcmp("--version", argv[i]) || !strcmp("-V", argv[i])) {
+			} else if (!strcmp("--version", argv[i]) || !strcmp("-V", argv[i])) {
 				printf(VERSION "\n");
 				return 0;
-			}
-			else if (argv[i][0] == '-') {
+			} else if (argv[i][0] == '-') {
 				printf("Usage: %s [--debug] [--exit] [--help] [--startup CMD] [--version]\n", argv[0]);
 				return strcmp("--help", argv[i]) != 0 && strcmp("-h", argv[i]) != 0;
 			}
